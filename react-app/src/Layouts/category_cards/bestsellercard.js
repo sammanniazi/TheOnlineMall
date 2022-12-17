4a20bd101122511af006923ec4b5ca061551f968
Samman Niazi <sammanurooj786@gmail.com>
2022-12-17 23:30:47 +0500

    cart implementation version2

--- react-app/src/Layouts/category_cards/bestsellercard.js
+++ react-app/src/Layouts/category_cards/bestsellercard.js
@@ -1,6 +1,8 @@
+import userEvent from "@testing-library/user-event";
 import { useState, useEffect } from "react";
 
 import Button from 'react-bootstrap/Button';
+import { json } from "react-router-dom";
 
 
 
@@ -8,12 +10,36 @@ import Button from 'react-bootstrap/Button';
 
 function Homecard() {
 
+  var myMap = new Map();
+
+
   const [data, getData] = useState([]);
+
+
+  
+  
+  function savecartdata(data){
+
+
+    var a=[];
+    localStorage.setItem("user", JSON.stringify(data))
+   
+    a.push(data);
+    console.log(a)
+    localStorage.getItem('user', JSON.stringify(a));
+ 
+
+
+
+  }
 
 
   const URL = "http://localhost:5000/bestSeller";
   useEffect(() => {
     fetchData();
+   
+  
+   
     
   }, []);
   {console.log(data.currentUser)}
@@ -38,22 +64,21 @@ function Homecard() {
      <div className="container-fluid mt-5">
                 <div className="row text-center">
        
-        {data.map((products) => (
+        {data.map((products,item) => (
                
-               <div className="col-10 col-md-4 mt-5" >
+               <div  key={item}  className="col-10 col-md-4 mt-5" >
                  <div className="card  "   >
                  <div className="d-flex align-items-center">
                   <img className="card-img-top" src={products.image} alt="Card image cap" class="rounded" width="250"/>
                   <div className="card-body">
                     <h5 className="card-title">{products.title.substring(0,12)}</h5>
                     <p className="card-text">${products.price}</p>
-                    <button onClick={() => localStorage.setItem("user", JSON.stringify(products))
+                    <button onClick={savecartdata(data)}
 
 
                     
         
-                  
-                  }>
+                >
               add to cart
             </button>
             
